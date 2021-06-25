e152c62c622967a611515de58db830439e3c348f
Gabriel Carvalho <gabriel57_pereira@hotmail.com>
2021-06-25 00:38:30 -0300

    nono Commit

---
+++ fundamentos/destructuring.js
@@ -0,0 +1,25 @@
+// novo recuso do ES2015
+
+const pessoa = {
+    nome: 'Ana',
+    idade: 5,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+
+const { nome, idade} = pessoa
+console.log(nome, idade)
+
+const { nome: n, idade: i } = pessoa
+console.log(n, i)
+
+const { sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada)
+
+const { endereco: { logradouro, numero, cep } } = pessoa
+console.log(logradouro, numero, cep)
+
+// const { conta: { ag, num } } = pessoa  - ERRO VARIAVEL NAO SETADA
+// console.log(ag, num) - ERRO VARIAVEL NAO SETADA
